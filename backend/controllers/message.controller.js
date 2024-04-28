61d35ab6f9e8386f763662f868a01bb7f6a21b33
Erick <ericksjp703@gmail.com>
2024-04-27 22:13:12 -0300

    Implementing sendMessage controller

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,1 +1,37 @@
-export const sendMessage = async (req, res) => {};
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
+
+    await Promise.all([newMessage.save(), conversation.save()]);
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
