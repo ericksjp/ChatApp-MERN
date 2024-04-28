89156ad7799a7eacc138070da28b59b99f407bf3
Erick <ericksjp703@gmail.com>
2024-04-27 21:41:11 -0300

    Creating protectRoute middleware

---
+++ backend/routes/message.route.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { sendMessage } from "../controllers/message.controller.js";
+import protectRoute from "../middleware/protectRoute.js";
+
+const router = Router();
+
+router.post("/send/:id", protectRoute, sendMessage);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { config } from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -9,8 +11,10 @@ const PORT = process.env.PORT || 3000;
 config();
 
 app.use(express.json()); // for parsing application/json
+app.use(cookieParser()); // for parsing cookies
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 // app.get("/", (req, res) => {
 //   res.send("Hello World");
